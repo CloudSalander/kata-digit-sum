957f85036f7ff90a6916a739b5e0d23f2c803e18
Cloud <ruben.as532@gmail.com>
2023-07-13 09:51:31 +0200

    sum digits seems to work

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
 function calcSumDigit(int $number): void {
-	
+	if($number == 0) echo "0 = 0, surprise motherfucker!".PHP_EOL;
+	else {
+		$digits = array();
+		$sum_digits = 0;
+		while($number > 0) {
+			$sum_digits += $number%10;
+			$number = intdiv($number,10);
+		}
+		echo " = ".$sum_digits;
+	}
 }
 
 $number = readline();
