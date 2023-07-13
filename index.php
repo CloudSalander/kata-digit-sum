4e3b824e58771f588f8c73d02245d9490d651ccc
Cloud <ruben.as532@gmail.com>
2023-07-13 09:58:05 +0200

    Now with output format, apparently complete

--- index.php
+++ index.php
@@ -1,14 +1,23 @@
 <?php
+function printDigits(array $digits) {
+	foreach ($digits as $index => $digit) {
+		echo $digit;
+		if($index < count($digits)-1) echo " + ";
+	}
+}
+
 function calcSumDigit(int $number): void {
 	if($number == 0) echo "0 = 0, surprise motherfucker!".PHP_EOL;
 	else {
 		$digits = array();
 		$sum_digits = 0;
 		while($number > 0) {
-			$sum_digits += $number%10;
+			$digit = $number%10;
+			$sum_digits += $digit;
+			array_unshift($digits, $digit);
 			$number = intdiv($number,10);
 		}
-		echo " = ".$sum_digits;
+		echo printDigits($digits)." = ".$sum_digits.PHP_EOL;
 	}
 }
 
